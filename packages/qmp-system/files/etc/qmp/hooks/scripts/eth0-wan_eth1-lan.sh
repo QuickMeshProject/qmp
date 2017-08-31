faa944dbdc19f60ffb4649d4a1a1fe84aade7880
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-08-31 15:30:10 +0200

    [qmp-system] Update hook for tl-wr841-v8 changes in LAN interface

--- packages/qmp-system/files/etc/qmp/hooks/scripts/eth0-wan_eth1-lan.sh
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/eth0-wan_eth1-lan.sh
@@ -3,8 +3,10 @@
 STAGE="$1"
 
 [ "$STAGE" == "birth" ] && {
-	echo "Configuring eth0 as wan and eth1 as lan"
-	uci set qmp.interfaces.lan_devices="eth1"
+	echo "Configuring eth0 as wan+mesh and eth1.1 as lan+mesh"
+	uci set qmp.interfaces.lan_devices="eth1.1"
+	uci set qmp.interfaces.ignore_devices="eth1"
 	uci set qmp.interfaces.wan_devices="eth0"
+	uci set qmp.interfaces.mesh_devices="eth0 eth1.1"
 	uci commit qmp
 }
