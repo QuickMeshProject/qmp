1c726297359bdb585c710e351016d0e5b8e9585d
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2018-06-04 20:42:17 +0200

    Update create_repository snippet
    Now the snippet uses the "output" directory by default and copies both
    packages and targets
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- snippets/create_repository.sh
+++ snippets/create_repository.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 . options.conf
 . options.conf.local
-OUTDIR="${1:-repository}"
+
+OUTDIR="${1:-output}"
 [ ! -d $OUTDIR ] && mkdir -p $OUTDIR
+[ ! -d $OUTDIR/packages ] && mkdir -p $OUTDIR/packages
+[ ! -d $OUTDIR/targers ] && mkdir -p $OUTDIR/targets
+
 echo "-> Output directory: $OUTDIR"
-for link in $OUTDIR/*; do
+
+for link in $OUTDIR/packages/*; do
   unlink $link 2>/dev/null
 done
+for link in $OUTDIR/targets/*; do
+  unlink $link 2>/dev/null
+done
+
 for target in $(cat $targets_list); do
   for arch in $release/$target/sdk/bin/packages/*; do
     [ -d "$arch" ] && {
-      echo "-> Creating symlink for $arch"
-      ln -s $PWD/$arch $OUTDIR/ 2>/dev/null
+      echo "-> Creating symlink for $arch packages"
+      ln -s $PWD/$arch $OUTDIR/packages/ 2>/dev/null
+    }
+  done
+  for arch in $release/$target/sdk/bin/targets/*; do
+    [ -d "$arch" ] && {
+      echo "-> Creating symlink for $arch targets"
+      ln -s $PWD/$arch $OUTDIR/targets/ 2>/dev/null
     }
   done
 done
