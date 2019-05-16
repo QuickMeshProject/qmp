a6855655292cc530af10905d5b6579d21ab31400
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2019-05-16 17:34:30 +0200

    [qmp-system] Allow adding new search and offer gateways
    New gateways can be added now, by using the section type
    "new_offer_gateway" or "new_search_gateway", which are converted to
    regular "gateway" sections on submitting the form.
    
    Fixes #295.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- packages/qmp-system/files/usr/lib/lua/luci/model/cbi/qmp/gateways_search.lua
+++ packages/qmp-system/files/usr/lib/lua/luci/model/cbi/qmp/gateways_search.lua
@@ -10,21 +10,24 @@ local uci = luci.model.uci.cursor()
 ------------
 -- Header --
 ------------
-m = Map("gateways", "qMp gateways", translate("This page allows configuring the type of tunnels or gateways this qMp node must search for from other nodes in the mesh, and which gateways (e.g., an Internet connection) can offer to the other nodes in the mesh..")
-  .. "<br/> <br/>"
-  .. translate("You can check the on-line documentation at <a href=\"https://www.qmp.cat/Web_interface\">https://www.qmp.cat/Web_interface</a> for more information about the different options."))
 
-new_gw_section = m:section(TypedSection, "new_gw",
-  translate("Gateways to search for"),
-  translate("Use this tab to configure the gateways and network announcements your node must fetch (e.g., to look for an Internet gateway, to fetch specific subnetwork announcements).")
-  .. "<br/> <br/><strong>"..translate("Use with care!").."</strong> "
-  .. translate("Publising incorrect announcements might cause other nodes to lose Internet connection or render the mesh network inoperative."))
+m = Map("gateways", "qMp gateways", translate("This page allows configuring the type of tunnels or gateways this qMp node must search for from other nodes in the mesh, and which gateways (e.g., an Internet connection) can offer to the other nodes in the mesh.")
+  .. "<br/> <br/>"
+  .. translate("Use the <strong>Gateways to search for</strong> tab to configure the gateways and network announcements this qMp node must search for and fetch (e.g., to look for an Internet gateway, to fetch specific subnetwork announcements) from other nodes in the mesh network.")
+  .. "<br/>"
+  .. translate("Use the <strong>Gateways to offer</strong> to publish the gateways and network routes this qMp node will announce and offer and make available to the rest of nodes in the mesh (e.g., to publush a route to a specific subnetwork, to offer a shared Internet connection, etc.).")
+  .. "<br/> <br/>"
+  .. "<strong>"..translate("Proceed with care!").."</strong>".." "
+  .. translate("Publising incorrect announcements might cause other nodes to lose Internet connection or render the mesh network inoperative.")
+  .. "<br/> <br/>"
+  .. translate("You can check the on-line documentation at <a href=\"https://www.qmp.cat/Web_interface\">https://www.qmp.cat/Web_interface</a> for more information about the different options.")
+  .. "<hr>")
 
 ----------------------
 -- Gateways section --
 ----------------------
 
--- Get the gateways to search for
+-- Get the names of the search gateways
 gw_names = {}
 uci:foreach('gateways', 'gateway', function (s)
   local name = s[".name"]
@@ -34,27 +37,8 @@ uci:foreach('gateways', 'gateway', function (s)
   end
 end)
 
-for _,gw in ipairs(gw_names) do
-
-  gw_section = m:section(NamedSection, gw, "Gateway section", 'Gateway "' .. gw .. '"')
-
-  -- Default parameters
-  ignore = gw_section:option(Flag,"ignore", "<b>"..translate("Enabled").."</b>", translate("Enable this rule"))
-  ignore.default="0"
-  ignore.disabled="1"
-  ignore.enabled="0"
-
-  type = gw_section:option(ListValue,"type", "<b>"..translate("Type").."</b>",translate("offer = announce some specific network in the mesh, search = look for some specific network"))
-  type:value("search","search")
-  -- type:value("offer","offer")
-  type.optional = false
-  type.hidden = true
-
-  network = gw_section:option(Value,"network", "<b>"..translate("Network").."</b>",translate("network to be offered/searched. NETWORK/PREFIX must be specified, for example 10.0.0.0/8 or 0.0.0.0/0"))
-  network.optional = false
-
-  -- Optional parameters (not using all BMX6 tun options)
-  local gwoptions = {
+-- Optional parameters for search gateways (not using all BMX6 tun options, though)
+local gwoptions = {
   {["name"]="address", ["help"]=""},
   {["name"]="gwName", ["help"]="force your node to choose a specific gateway by specifying its hostname (such as qMp0012)"},
   {["name"]="minPrefixLen", ["help"]="minimum prefix length for accepting advertised tunnel network (value 129 = network prefix length)"},
@@ -71,7 +55,27 @@ for _,gw in ipairs(gw_names) do
   {["name"]="tableRule", ["help"]="ip rules tabel and preference to maintain matching tunnels ({PREF}/{TABLE})"},
   {["name"]="ipMetric", ["help"]="ip metric for local routing table entries"},
   {["name"]="hysteresis", ["help"]="specify in percent how much the metric to an alternative GW must be better than current GW"}
-  }
+}
+
+-- Display all the gateways to search for
+for _,gw in ipairs(gw_names) do
+  gw_section = m:section(NamedSection, gw, 'gateway', 'Search for gateway "' .. gw .. '"')
+  gw_section.addremove = true
+  gw_section.anonymous = false
+
+  -- Add their default parameters
+  ignore = gw_section:option(Flag,"ignore", "<b>"..translate("Enabled").."</b>", translate("Enable this rule"))
+  ignore.default="0"
+  ignore.disabled="1"
+  ignore.enabled="0"
+
+  type = gw_section:option(ListValue,"type", "<b>"..translate("Type").."</b>",translate("offer = announce some specific network in the mesh, search = look for some specific network"))
+  type:value("search","search")
+  type.optional = false
+  type.hidden = true
+
+  network = gw_section:option(Value,"network", "<b>"..translate("Network").."</b>",translate("network to be offered/searched. NETWORK/PREFIX must be specified, for example 10.0.0.0/8 or 0.0.0.0/0"))
+  network.optional = false
 
   for _,o in ipairs(gwoptions) do
   	if o.name ~= nil then
@@ -81,11 +85,103 @@ for _,gw in ipairs(gw_names) do
   		value:depends("type","search")
   	end
   end
+end
 
+--------------------------
+-- New gateways section --
+--------------------------
+
+new_gw_section = m:section(TypedSection, "new_search_gateway",
+translate("Add new gateways and routes to search for"),
+translate("Use this option to add new gateways this qMp node will search for and fetch from the mesh network (e.g., to look for an Internet gateway, to fetch specific subnetwork announcements).")
+.."<br/>"
+..translate('Use a <strong>unique name</strong> for the new gateway section, which is not being used neither in this tab or in the "Offered gateways" tab.'))
+
+new_gw_section.addremove = true
+new_gw_section.anonymous = false
+
+-- Add their default parameters
+ignore = new_gw_section:option(Flag,"ignore", "<b>"..translate("Enabled").."</b>", translate("Enable this rule"))
+ignore.default="1"
+ignore.disabled="1"
+ignore.enabled="0"
+
+type = new_gw_section:option(ListValue,"type", "<b>"..translate("Type").."</b>",translate("offer = announce some specific network in the mesh, search = look for some specific network"))
+type:value("search","search")
+type.optional = false
+
+network = new_gw_section:option(Value,"network", "<b>"..translate("Network").."</b>",translate("network to be offered/searched. NETWORK/PREFIX must be specified, for example 10.0.0.0/8 or 0.0.0.0/0"))
+network.optional = false
+
+for _,o in ipairs(gwoptions) do
+  if o.name ~= nil then
+    value = new_gw_section:option(Value, o.name, translate(o.name), translate(o.help))
+    value.optional = true
+    value.rmempty = true
+    value:depends("type","search")
+  end
 end
 
 function m.on_commit(self,map)
+  uci:commit("gateways")
   http.redirect("/luci-static/resources/qmp/wait_short.html")
+
+  ogw = uci.cursor()
+  ngw = uci.cursor()
+  tgw = uci.cursor()
+
+  ngw:foreach("gateways", "new_search_gateway", function(n)
+    local nname = n[".name"]
+
+    -- search for an existing gateway with the same name before converting section type
+    local dup = false
+    local dupname = ""
+
+    ogw:foreach("gateways", 'gateway', function (o)
+      local oname = o[".name"]
+      if (nname == oname) then
+        dup = true
+      end
+    end)
+
+    while (dup)
+    do
+      dupname = dupname .. "_dup"
+      local this_dup = false
+      ogw:foreach("gateways", 'gateway', function (o)
+        local oname = o[".name"]
+        local nname_dup = nname..dupname
+        if (nname_dup == oname) then
+          this_dup = true
+        end
+      end)
+      dup = this_dup
+    end
+
+    ogw:set("gateways", nname..dupname, "gateway")
+    ogw:set("gateways", nname..dupname, "ignore",
+      tgw:get('gateways', nname, 'ignore'))
+    ogw:set("gateways", nname..dupname, "type",
+      tgw:get('gateways', nname, 'type'))
+    ogw:set("gateways", nname..dupname, "network",
+      tgw:get('gateways', nname, 'network'))
+
+      for _,op in ipairs(gwoptions) do
+        if op.name ~= nil then
+          local opvalue = tgw:get("gateways", nname, op.name)
+          if opvalue ~= nil then
+            ogw:set("gateways", nname..dupname, op.name, opvalue)
+          end
+        end
+      end
+
+  end)
+
+  ngw:foreach("gateways", "new_search_gateway", function(n)
+    local nname = n[".name"]
+    tgw:delete("gateways", nname)
+  end)
+
   uci:commit("gateways")
 	luci.sys.call('/etc/qmp/qmp_control.sh configure_gw > /tmp/qmp_control_system.log &')
 end
